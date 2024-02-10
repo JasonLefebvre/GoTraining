33ded83a9fdb6af9c05336e5c2e0bf951fe9f198
JasonLefebvre <jasonlefebvre25septembre@gmail.com>
2024-02-10 13:29:42 +0100

    update

--- main.go
+++ main.go
@@ -1,16 +1,1 @@
 package main
-
-import (
-	"fmt"
-)
-
-func main() {
-
-	var x int // Type après l'identifiant
-	x = 15    // Variable assignée après la déclaration
-
-	y := 18 // Variable déclarée et assignée | fonctionne que dans une fonction
-
-	fmt.Printf("Mon nombre est: %v!", x)
-	fmt.Printf("Mon nombre est: %v!", y)
-}
